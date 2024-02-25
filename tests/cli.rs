bf50bbd26128e664eef9d721c40e107a54fb84c4
T4D4 <t4d4.icp@gmail.com>
2024-02-25 18:12:48 +0900

    add result type

--- tests/cli.rs
+++ tests/cli.rs
@@ -2,10 +2,21 @@ use assert_cmd::Command;
 use predicates::prelude::*;
 use std::fs;
 
+type TestResult = Result<(), Box<dyn std::error::Error>>; // テスト関数の戻り値の型エイリアスの作成。
+
 #[test]
-fn  dies_no_args() {
-    let outfile = "tests/expected/hello1.txt";
-    let expected = fs::read_to_string(outfile).unwrap();
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn  dies_no_args() -> TestResult {
+    Command::cargo_bin("echor")? // Ok値のアンパックやErr値の伝播の為にunwrapの代わりに?を使っている
+        .assert()
+        .failure()
+        .stderr(predicate::str::contains("USAGE"));
+    Ok(()) // Ok値を返す為に;を省略している
+}
+
+#[test]
+fn hello() -> TestResult {
+    let expected = fs::read_to_string("tests/expected/hello.txt")?;
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.arg("Hello there").assert().success().stdout(expected);
+    Ok(())
 }
